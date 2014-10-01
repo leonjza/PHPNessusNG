57ada96f904cd4435edc5bbbfb01dca73a222895
Otto Odgaard <oodgaard@ultraserve.com.au>
2014-10-01 11:38:00 +1000

    handle a single policy being returned. added array checking for the policy list response.

--- src/Nessus/NessusInterface.php
+++ src/Nessus/NessusInterface.php
@@ -311,8 +311,15 @@ class NessusInterface
 
         //##ENHANCEMENT: Lots more information available here. Should maybe make a seperate details() call.
         $values = array();
-        foreach ($response->policies->policy as $policy)
+
+        if (is_array($response->policies->policy)) {
+            foreach ($response->policies->policy as $policy) {
+                $values['policies'][$policy->policyid] = $policy->policyname;
+            }
+        } elseif (is_object($response->policies->policy)) {
+            $policy = $response->policies->policy;
             $values['policies'][$policy->policyid] = $policy->policyname;
+        }
 
         //Return what we got
         return $values;
