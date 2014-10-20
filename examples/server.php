497be5d3ee40c01d2e277ae0e7b5d5ce25814246
Leon Jacobs <leonja511@gmail.com>
2014-10-20 14:25:12 +0200

    Add example for server calls

---
+++ examples/server.php
@@ -0,0 +1,27 @@
+<?php
+
+include '../vendor/autoload.php';
+
+// Prepare the connection to the API
+$nessus = new Nessus\Client('username', 'password', '192.168.56.101');
+
+// Get the Server properties
+$server_properties = $nessus->server()->properties()->via('get');
+
+print '[+] Server Version: ' . $server_properties->server_version . PHP_EOL;
+print '[+] Feed: ' . $server_properties->feed . PHP_EOL;
+foreach ($server_properties->notifications as $notification)
+    print '[+] Notification Type: ' . $notification->type . ' : ' . $notification->message . PHP_EOL;
+
+$server_status = $nessus->server()->status()->via('get');
+print '[+] Server Progress: ' . $server_status->progress . PHP_EOL;
+print '[+] Server Status: ' . $server_status->status . PHP_EOL;
+
+
+// Sample output
+// λ git n6* → php server.php
+// [+] Server Version: 6.0.0
+// [+] Feed: ProFeed
+// [+] Notification Type: warning : Your plugin feed subscription will expire in 26 day(s).
+// [+] Server Progress:
+// [+] Server Status: ready
